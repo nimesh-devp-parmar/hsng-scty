9b2e406ece00fb9c1bd18af267809314cece5795
Applix Technologies - Nimesh Parmar <nimesh.devp.parmar@gmail.com>
2025-09-12 10:26:10 -0500

    Added an API Gateway with all routing parameters in application.yml file

---
+++ src/main/java/com/applix/hsngsctyapig/HsngSctyApigApplication.java
@@ -0,0 +1,13 @@
+package com.applix.hsngsctyapig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsngSctyApigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsngSctyApigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/applix/hsngsctyapig/HsngSctyApigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applix.hsngsctyapig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsngSctyApigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
